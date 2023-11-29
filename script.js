d1a3c19bf72923be6012c6c0daa4a4e831cdd1d5
Nikki-Terrones <Nikkistixs@yahoo.com>
2023-11-28 21:48:26 -0800

    made clock change by the second

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-// invoke each function ()
+// Video I was able to understand and follow along 
+// https://youtu.be/2glfqa-ZbNw?si=RBKsLBd85NOQR4Hr
 
+function updateClock(){
+  const now = new Date();
+  let hours = now.getHours();
+  const meridiem = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12;
+  hours = hours.toString().padStart(2, 0);
+  const minutes = now.getMinutes().toString().padStart(2, 0);
+  const seconds = now.getSeconds().toString().padStart(2, 0);
+  const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
+  document.getElementById("clockFace").textContent = timeString;
+}
 
-
-
-
-
-
-
-
-
-
-document.getElementById('clockFace').innerHTML = hour+':'+min+':'+sec +' '+mid;
-  setTimeout(clock, 1000);
+updateClock();
+setInterval(updateClock, 1000);
