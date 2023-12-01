99245ca73a103c35a145489ae3a796ccbdc8c1d1
Nikki-Terrones <Nikkistixs@yahoo.com>
2023-11-30 21:59:48 -0800

    put in example of date format in js

--- script.js
+++ script.js
@@ -15,3 +15,29 @@ function updateClock(){
 
 updateClock();
 setInterval(updateClock, 1000);
+
+// make the date
+// line it up horizontally
+// change text color to stand out
+// One Step At A Time :)
+
+// function(){
+//   const newDate = new Date(month, day, year);
+//   let month = newDate.getMonth();
+//   const day = newDate.getDay();
+//   const year = newDate.getYear();
+//   const dateString = `${month},${day}.${year}`;
+// }
+
+// updateDate();
+// setInterval(updateDate, 60); 
+// document.getElementById("date").textContent
+
+// var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+// var today  = new Date();
+
+// console.log(today.toLocaleDateString("en-US", options));
+
+// var dateFormat = require('dateformat');
+// var now = new Date();
+// dateFormat(now, "mmmm dS, yyyy");
